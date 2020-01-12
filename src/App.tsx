34c18ba79cec050747fbc3630c3d4144846f17d8
arx-8 <swans.randomness@gmail.com>
2020-01-12 12:51:14 +0900

    chore: Absolute path import settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-import "./App.css"
-import logo from "./logo.svg"
+import "src/App.css"
+import logo from "src/logo.svg"
 
 export const App: React.FC = () => {
   return (
