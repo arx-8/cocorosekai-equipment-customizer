3e00be42d08e0b9dd53ad13e615ca9e983b1d939
arx-8 <swans.randomness@gmail.com>
2020-01-12 20:18:01 +0900

    feat: base

---
+++ src/components/organisms/CustomizeTable/index.tsx
@@ -0,0 +1,13 @@
+/** @jsx jsx */
+import React from "react"
+import { css, jsx } from "@emotion/core"
+
+type OwnProps = {
+  children?: never
+}
+
+export const CustomizeTable: React.FC<OwnProps> = () => {
+  return <div css={root}>TODO</div>
+}
+
+const root = css``
---
+++ src/components/organisms/ItemsTable/index.tsx
@@ -0,0 +1,13 @@
+/** @jsx jsx */
+import React from "react"
+import { css, jsx } from "@emotion/core"
+
+type OwnProps = {
+  children?: never
+}
+
+export const ItemsTable: React.FC<OwnProps> = () => {
+  return <div css={root}>TODO</div>
+}
+
+const root = css``
